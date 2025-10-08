4505fbfba994795337602fabe3ea0f2dab8e0c45
Mrinmoy Sadhukhan <72397091+mrinmoy-sadhukhan@users.noreply.github.com>
2025-10-08 22:51:53 +0530

    Update hybriddetr.py

--- models/hybriddetr.py
+++ models/hybriddetr.py
@@ -549,7 +549,7 @@ class SwinUNetMultiUp(nn.Module):
     def forward(self, x):
         # Swin features
         features = self.backbone(x)
-        print(features[2].shape,features[3].shape,features[4].shape)
+        print(features[0].shape,features[1].shape,features[2].shape)
         for i in range(2, 5): #1 to 4
             if features[i].shape[1] < features[i].shape[-1]:  # channels last
                 features[i] = features[i].permute(0, 3, 1, 2).contiguous()
